cd8bb0115e7ca22a9903f290252e3416d81e099c
vjsreekanth <vjsreekanth@gmail.com>
2024-07-20 21:22:35 -0700

    Triangle Classifier homework completed

---
+++ src/Homeworks/TriangleClassifier.java
@@ -0,0 +1,26 @@
+package Homeworks;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class TriangleClassifier {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter side one length");
+        int side_a = sc.nextInt();
+        System.out.println("Enter side two length");
+        int side_b = sc.nextInt();
+        System.out.println("Enter side three length");
+        int side_c = sc.nextInt();
+        if(side_a == side_b && side_a == side_c){
+            System.out.println("equilateral triangle");
+
+        }else if(side_a == side_b || side_a == side_c || side_b == side_c){
+            System.out.println("Isosceles Triangle");
+        }else{
+            System.out.println("Scalene Triangle");
+        }
+
+        sc.close();
+    }
+}
